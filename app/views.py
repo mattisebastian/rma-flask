bc2834c5a5ac9b59c335abf73e95c070a8379ded
mattisebastian <matti.leydecker@gmx.de>
2015-09-03 00:35:58 +0200

    mega-tutorial-part3

--- app/views.py
+++ app/views.py
@@ -1,6 +1,29 @@
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'Matti'} # fake user
+	posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+	return render_template('index.html',
+							title = 'Home',
+							user = user,
+							posts = posts)
